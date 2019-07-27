829b8f72b66ec47a798dd8f9effe5bbd345fdfeb
LiuWenYuan <15816141883@163.com>
2019-07-27 13:12:19 +0800

    Delete Load event.

--- AboutBox.Designer.cs
+++ AboutBox.Designer.cs
@@ -169,7 +169,6 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "AboutBox";
-            this.Load += new System.EventHandler(this.AboutBox_Load);
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
